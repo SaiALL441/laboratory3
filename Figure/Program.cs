47488ce41856fa19623cdcbafef97e922b976bb1
Александра <112776816+SaiALL441@users.noreply.github.com>
2022-10-07 23:13:46 +0300

    Add files via upload

---
+++ Figure/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figure
+{
+    public class Point
+    {
+        
+        private int a;
+        private int b;
+        private int c;
+        private int d;
+        private string s;
+        public int Side1 
+        { get 
+            { return a; 
+            }
+            set
+            {
+                a = value;
+            }
+        }
+        public int Side2
+        {
+            get
+            {
+                return b;
+            }
+            set
+            {
+                b = value;
+            }
+        }
+        public int Side3
+        {
+            get
+            {
+                return c;
+            }
+            set
+            {
+                c = value;
+            }
+        }
+        public int Side4
+        {
+            get
+            {
+                return d;
+            }
+            set
+            {
+                d = value;
+            }
+        }
+        public string Name
+        {
+            set 
+            { s = value; }
+        }
+       
+        public int PerimeterCalculator
+        {
+            get
+            {
+                return (a + b + c + d);
+            }
+        }
+        public void Print()
+        {
+            Console.WriteLine($"{s} {a}x{b}x{c}x{d} Периметр = {PerimeterCalculator}");
+        }
+       
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point r = new Point();
+            r.Side1 = 6;
+            r.Side2 = 5;
+            r.Side3 = 3;
+            r.Side4 = 4;
+            r.Name = "ABCD";
+            r.Print(); 
+            
+        }
+    }
+}
